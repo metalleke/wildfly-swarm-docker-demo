0aea616ef2c436800286de3009ea8d08a886399c
Kevin Van Raepenbusch <metalleke@gmail.com>
2018-01-09 23:34:57 +0100

    Added sonar plugin. Corrected package name.

--- src/main/java/net/metalleke/demo/MyApplication.java
+++ src/main/java/net/metalleke/demo/MyApplication.java
@@ -1,4 +1,4 @@
-package net.metalleke.surf;
+package net.metalleke.demo;
 
 import java.util.HashSet;
 import java.util.Set;
--- src/main/java/net/metalleke/demo/MyResource.java
+++ src/main/java/net/metalleke/demo/MyResource.java
@@ -1,4 +1,4 @@
-package net.metalleke.surf;
+package net.metalleke.demo;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
